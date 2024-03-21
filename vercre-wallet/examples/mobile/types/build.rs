514cdd76792483e911c844bdba0adb5d2b96c91a
andrewweston <andrew.weston@propellerhead.co.nz>
2024-03-21 14:35:00 +1300

    remove undeveloped wallet examples

--- vercre-wallet/examples/mobile/types/build.rs
+++
@@ -1,9 +0,0 @@
-//! Generate UI-specific types used by each user interface to communicate with the
-//! `vercre-wallet` core.
-
-use vercre_wallet::typegen::{self, Language};
-
-fn main() {
-    typegen::generate(Language::Java, "./gen");
-    typegen::generate(Language::Swift, "./gen");
-}
--- vercre-wallet/examples/wasm/src/components/mod.rs
+++
@@ -1,6 +0,0 @@
-mod container;
-mod stack;
-pub(crate) mod types;
-
-pub use container::Container;
-pub use stack::Stack;
