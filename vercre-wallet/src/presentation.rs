70b67b5b4a62bce235f22d180cff1dab8a62c8d4
Andrew Goldie <andrew.goldie@goldiesystems.com>
2024-06-05 17:11:06 +1200

    fix ser/de of presentation request object as jwt

--- vercre-wallet/src/presentation.rs
+++ vercre-wallet/src/presentation.rs
@@ -272,11 +272,14 @@ fn parse_request_object_response(res: &RequestObjectResponse) -> Result<RequestO
         return Ok(res.request_object.clone().unwrap());
     }
     let Some(jwt_enc) = res.jwt.clone() else {
-        err!(Err::InvalidRequest, "no encoded JWT found in response");
+        err!(Err::InvalidRequest, "no serialized JWT found in response");
     };
-    let Ok(jwt) = jwt_enc.parse::<Jwt<RequestObject>>() else {
+    let Ok(jwt) = serde_json::from_str::<Jwt<RequestObject>>(&jwt_enc) else {
         err!(Err::InvalidRequest, "failed to parse JWT");
     };
+    // let Ok(jwt) = jwt_enc.parse::<Jwt<RequestObject>>() else {
+    //     err!(Err::InvalidRequest, "failed to parse JWT");
+    // };
 
     Ok(jwt.claims)
 }
@@ -370,8 +373,9 @@ mod tests {
     use std::collections::HashMap;
 
     // use insta::assert_yaml_snapshot as assert_snapshot;
-    use vercre_core::w3c::{Field, Filter, FilterValue, Format, InputDescriptor, PresentationDefinition};
-    use vercre_vci::jwt::Header;
+    use vercre_core::w3c::{
+        Field, Filter, FilterValue, Format, InputDescriptor, PresentationDefinition,
+    };
     use vercre_vp::provider::Algorithm;
 
     use super::*;
@@ -435,20 +439,20 @@ mod tests {
     #[test]
     fn parse_request_object_response_test() {
         let obj = sample_request();
-        let jwt = Jwt::<RequestObject> {
-            header: Header {
-                typ: "JWT".into(),
-                alg: "ES256K".into(),
-                kid: "did:ion:EiDyOQbbZAa3aiRzeCkV7LOx3SERjjH93EXoIM3UoN4oWg:eyJkZWx0YSI6eyJwYXRjaGVzIjpbeyJhY3Rpb24iOiJyZXBsYWNlIiwiZG9jdW1lbnQiOnsicHVibGljS2V5cyI6W3siaWQiOiJwdWJsaWNLZXlNb2RlbDFJZCIsInB1YmxpY0tleUp3ayI6eyJjcnYiOiJzZWNwMjU2azEiLCJrdHkiOiJFQyIsIngiOiJ0WFNLQl9ydWJYUzdzQ2pYcXVwVkpFelRjVzNNc2ptRXZxMVlwWG45NlpnIiwieSI6ImRPaWNYcWJqRnhvR0otSzAtR0oxa0hZSnFpY19EX09NdVV3a1E3T2w2bmsifSwicHVycG9zZXMiOlsiYXV0aGVudGljYXRpb24iLCJrZXlBZ3JlZW1lbnQiXSwidHlwZSI6IkVjZHNhU2VjcDI1NmsxVmVyaWZpY2F0aW9uS2V5MjAxOSJ9XSwic2VydmljZXMiOlt7ImlkIjoic2VydmljZTFJZCIsInNlcnZpY2VFbmRwb2ludCI6Imh0dHA6Ly93d3cuc2VydmljZTEuY29tIiwidHlwZSI6InNlcnZpY2UxVHlwZSJ9XX19XSwidXBkYXRlQ29tbWl0bWVudCI6IkVpREtJa3dxTzY5SVBHM3BPbEhrZGI4Nm5ZdDBhTnhTSFp1MnItYmhFem5qZEEifSwic3VmZml4RGF0YSI6eyJkZWx0YUhhc2giOiJFaUNmRFdSbllsY0Q5RUdBM2RfNVoxQUh1LWlZcU1iSjluZmlxZHo1UzhWRGJnIiwicmVjb3ZlcnlDb21taXRtZW50IjoiRWlCZk9aZE10VTZPQnc4UGs4NzlRdFotMkotOUZiYmpTWnlvYUFfYnFENHpoQSJ9fQ#publicKeyModel1Id".into(),
-                ..Default::default()
-            },
-            claims: obj.clone(),
-        };
         let req_obj_res = RequestObjectResponse {
             request_object: Some(obj.clone()),
-            jwt: Some(jwt.to_string()),
+            jwt: None,
         };
-        let decoded = parse_request_object_response(&req_obj_res).expect("should parse");
+        let decoded =
+            parse_request_object_response(&req_obj_res).expect("should parse with object");
+        assert_eq!(obj, decoded);
+        let jwt = obj.to_jwt().expect("should serialize to jwt");
+        let jwt_str = serde_json::to_string(&jwt).expect("should serialize jwt");
+        let req_obj_res = RequestObjectResponse {
+            request_object: None,
+            jwt: Some(jwt_str),
+        };
+        let decoded = parse_request_object_response(&req_obj_res).expect("should parse with jwt");
         assert_eq!(obj, decoded);
     }
 }
