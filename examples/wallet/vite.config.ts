15a3bfc3501bef987491b8b7889f34c27a995dd9
Andrew Goldie <andrew.goldie@goldiesystems.com>
2024-06-06 10:30:42 +1200

    empty tauri app

---
+++ examples/wallet/vite.config.ts
@@ -0,0 +1,56 @@
+// import { fileURLToPath, URL } from 'node:url'
+
+import process from 'process';
+
+import react from "@vitejs/plugin-react";
+import { internalIpV4 } from "internal-ip";
+import { defineConfig } from "vite";
+
+const mobile =process.env.TAURI_PLATFORM === "android" ||
+    process.env.TAURI_PLATFORM === "ios";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+
+    // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+    // prevent vite from obscuring rust errors
+    clearScreen: false,
+    // tauri expects a fixed port, fail if that port is not available
+    server: {
+        host: mobile ? "0.0.0.0" : false,
+        port: 5173,
+        hmr: mobile
+            ? {
+                protocol: "ws",
+                host: await internalIpV4(),
+                port: 5173,
+            }
+            : undefined,
+        strictPort: true,
+        watch: {
+            // 3. tell vite to ignore watching `src-tauri`
+            ignored: ["**/src-tauri/**"],
+        },
+    },
+    // optimizeDeps: {
+    //     include: ['@mui/material/Unstable_Grid2'],
+    // },
+    // to make use of `TAURI_DEBUG` and other env variables
+    // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
+    envPrefix: ["VITE_", "TAURI_"],
+    build: {
+        // Tauri supports es2021
+        target: process.env.TAURI_PLATFORM === "windows" ? "chrome105" : "safari13",
+        // don't minify for debug builds
+        minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+        // produce sourcemaps for debug builds
+        sourcemap: !!process.env.TAURI_DEBUG,
+    },
+    resolve: {
+        alias: { '@': '/src' },
+    },
+    // resolve: {
+    //     alias: [{ find: '@', replacement: '/src' }],
+    // },
+});
