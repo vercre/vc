1e56881d8cbb6c6ca8494e5d53092ded8b9dd35a
andrewweston <andrew.weston@propellerhead.co.nz>
2024-06-09 19:24:53 +1200

    bump deploy

--- vercre-issuer/src/authorize.rs
+++ vercre-issuer/src/authorize.rs
@@ -63,7 +63,7 @@
 //! [RFC9396]: (https://www.rfc-editor.org/rfc/rfc9396)
 
 // TODO: add support for "ldp_vc" format
-// LATER: add support for "jwt_vc_json-ld" format
+// TODO: add support for "jwt_vc_json-ld" format
 // LATER: add support for "vc+sd-jwt" format
 // LATER: add support for "mso_mdoc" format
 // LATER: implement `Interval` and `SlowDown` checks/errors
