d9ebc09c5c180d0f931b03ecc941279b17644b5d
andrewweston <andrew.weston@propellerhead.co.nz>
2024-06-17 20:57:49 +1200

    remove unused code

--- examples/providers/src/lib.rs
+++ examples/providers/src/lib.rs
@@ -4,22 +4,4 @@ pub mod issuance;
 pub mod presentation;
 pub mod wallet;
 
-use std::sync::Once;
 
-use tracing::Level;
-use tracing_subscriber::FmtSubscriber;
-
-// initalise tracing once for all tests
-static INIT: Once = Once::new();
-
-/// Initialise tracing for tests.
-///
-/// # Panics
-///
-/// Panics if the tracing subscriber cannot be set.
-pub fn init_tracer() {
-    INIT.call_once(|| {
-        let subscriber = FmtSubscriber::builder().with_max_level(Level::ERROR).finish();
-        tracing::subscriber::set_global_default(subscriber).expect("subscriber set");
-    });
-}
