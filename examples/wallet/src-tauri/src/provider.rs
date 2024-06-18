cf3289613bd5cec262cb36ad30c7a9b0350079ff
Andrew Goldie <andrew.goldie@goldiesystems.com>
2024-06-18 14:46:43 +1200

    use shared example wallet provider

--- examples/wallet/src-tauri/src/provider.rs
+++ examples/wallet/src-tauri/src/provider.rs
@@ -2,26 +2,32 @@ use std::collections::HashMap;
 use std::sync::Arc;
 
 use futures::lock::Mutex;
-use vercre_holder::provider::{Callback, Payload};
+use providers::wallet::Provider as ExampleWalletProvider;
+use vercre_holder::provider::{Algorithm, Callback, Jwk, Payload, Signer, Verifier};
 
 pub mod issuer_client;
-pub mod signer;
 pub mod state;
 pub mod store;
 
 #[derive(Clone, Debug)]
+#[allow(clippy::struct_field_names)]
 pub struct Provider {
     app_handle: tauri::AppHandle,
     store: Arc<Mutex<HashMap<String, Vec<u8>>>>,
+    wallet_provider: ExampleWalletProvider,
 }
 
 impl Provider {
     /// Create a new capability provider.
     #[must_use]
-    pub const fn new(
+    pub fn new(
         app_handle: tauri::AppHandle, store: Arc<Mutex<HashMap<String, Vec<u8>>>>,
     ) -> Self {
-        Self { app_handle, store }
+        Self {
+            app_handle,
+            store,
+            wallet_provider: ExampleWalletProvider::new(),
+        }
     }
 }
 
@@ -29,5 +35,27 @@ impl Provider {
 impl Callback for Provider {
     async fn callback(&self, _pl: &Payload) -> anyhow::Result<()> {
         Ok(())
+    }
+}
+
+/// Hardcoded implementation of the `Signer` trait. **Do not use in production**
+impl Signer for Provider {
+    fn algorithm(&self) -> Algorithm {
+        Signer::algorithm(&self.wallet_provider)
+    }
+
+    fn verification_method(&self) -> String {
+        Signer::verification_method(&self.wallet_provider)
+    }
+
+    async fn try_sign(&self, msg: &[u8]) -> anyhow::Result<Vec<u8>> {
+        Signer::try_sign(&self.wallet_provider, msg).await
+    }
+}
+
+/// Hardcoded implementation of the `Verifier` trait. **Do not use in production**
+impl Verifier for Provider {
+    async fn deref_jwk(&self, did_url: &str) -> anyhow::Result<Jwk> {
+        Verifier::deref_jwk(&self.wallet_provider, did_url).await
     }
 }
