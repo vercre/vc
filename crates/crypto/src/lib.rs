212f14dd63ee2b55534b6d9f3a9c513d6ed88647
andrewweston <andrew.weston@propellerhead.co.nz>
2024-06-26 07:38:04 +1200

    simplify keyring structure

--- crates/crypto/src/lib.rs
+++ crates/crypto/src/lib.rs
@@ -6,23 +6,25 @@
 
 pub mod local;
 
-use signature::Keypair;
+// use signature::Keypair;
 
 /// A `Keyring` contains a set of related keys
 pub trait Keyring {
-    fn generate(&self, name: &str, alg: &Algorithm) -> anyhow::Result<impl Keypair>;
+    type VerifyingKey;
 
-    fn keypair(&self, name: &str) -> anyhow::Result<impl Keypair>;
+    fn generate(&self, name: &str) -> anyhow::Result<Self::VerifyingKey>;
 
     fn sign(&self, name: &str, data: &[u8]) -> anyhow::Result<Vec<u8>>;
+
+    fn verifying_key(&self, name: &str) -> anyhow::Result<Self::VerifyingKey>;
 
     // fn verify(&self, name: &str, data: &[u8], signature: &[u8]) -> anyhow::Result<()>;
 }
 
-pub enum Algorithm {
-    Ed25519,
-    X25519,
-}
+// pub enum Algorithm {
+//     Ed25519,
+//     X25519,
+// }
 
 // pub(crate) mod private {
 //     use super::*;
--- crates/crypto/src/local.rs
+++ crates/crypto/src/local.rs
@@ -2,60 +2,60 @@ use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 
 use anyhow::anyhow;
-// use ed25519_dalek::SigningKey;
 use rand::rngs::OsRng;
-use signature::{Keypair, Signer, Verifier};
+use signature::Signer;
 
-use super::{Algorithm, Keyring};
+// use ed25519_dalek::SigningKey;
+// use signature::{Keypair, Signer, Verifier};
+use super::Keyring;
 
-pub struct KeyStore {
-    keypairs: Arc<Mutex<HashMap<String, MyKeypair>>>,
+pub struct Ed25519Keyring {
+    keys: Arc<Mutex<HashMap<String, ed25519_dalek::SigningKey>>>,
 }
 
-#[derive(Clone)]
-pub struct MyKeypair {
-    signing_key: ed25519_dalek::SigningKey,
-}
+// #[derive(Clone)]
+// pub struct MyKeypair {
+//     signing_key: ed25519_dalek::SigningKey,
+// }
 
-impl Keypair for MyKeypair {
+// impl Keypair for MyKeypair {
+//     type VerifyingKey = ed25519_dalek::VerifyingKey;
+
+//     fn verifying_key(&self) -> Self::VerifyingKey {
+//         self.signing_key.verifying_key()
+//     }
+// }
+
+impl Keyring for Ed25519Keyring {
     type VerifyingKey = ed25519_dalek::VerifyingKey;
 
-    fn verifying_key(&self) -> Self::VerifyingKey {
-        self.signing_key.verifying_key()
-    }
-}
+    fn generate(&self, name: &str) -> anyhow::Result<Self::VerifyingKey> {
+        let signing_key = ed25519_dalek::SigningKey::generate(&mut OsRng);
 
-impl Keyring for KeyStore {
-    fn generate(&self, name: &str, alg: &Algorithm) -> anyhow::Result<impl Keypair> {
-        let keypair = MyKeypair {
-            signing_key: ed25519_dalek::SigningKey::generate(&mut OsRng),
-        };
+        let mut lock = self.keys.lock().map_err(|e| anyhow!("could not lock Keyring: {e}"))?;
+        lock.insert(name.to_string(), signing_key.clone());
 
-        let mut lock = self.keypairs.lock().map_err(|e| anyhow!("could not lock Keyring: {e}"))?;
-        lock.insert(name.to_string(), keypair.clone());
-
-        Ok(keypair)
-    }
-
-    fn keypair(&self, name: &str) -> anyhow::Result<impl Keypair> {
-        let lock = self.keypairs.lock().map_err(|e| anyhow!("could not lock Keyring: {e}"))?;
-        let Some(kp) = lock.get(name) else {
-            return Err(anyhow!("key not found"));
-        };
-
-        return Ok(kp.clone());
+        Ok(signing_key.verifying_key())
     }
 
     fn sign(&self, name: &str, data: &[u8]) -> anyhow::Result<Vec<u8>> {
-        // let signing_key = ed25519_dalek::SigningKey::from_bytes(&bytes);
-
-        let lock = self.keypairs.lock().map_err(|e| anyhow!("could not lock Keyring: {e}"))?;
-        let Some(kp) = lock.get(name) else {
+        let lock = self.keys.lock().map_err(|e| anyhow!("could not lock Keyring: {e}"))?;
+        let Some(signing_key) = lock.get(name) else {
             return Err(anyhow!("key not found"));
         };
 
-        let signature: ed25519_dalek::Signature = kp.signing_key.try_sign(data)?;
+        let signature: ed25519_dalek::Signature = signing_key.try_sign(data)?;
+
         Ok(signature.to_vec())
+    }
+
+    fn verifying_key(&self, name: &str) -> anyhow::Result<Self::VerifyingKey> {
+        let lock = self.keys.lock().map_err(|e| anyhow!("could not lock Keyring: {e}"))?;
+        let Some(signing_key) = lock.get(name) else {
+            return Err(anyhow!("key not found"));
+        };
+
+        return Ok(signing_key.verifying_key());
     }
 
     // fn verify(&self, name: &str, data: &[u8], signature: &[u8]) -> anyhow::Result<()> {
@@ -77,13 +77,13 @@ mod test {
 
     #[test]
     fn keyring() {
-        let keyring = KeyStore {
-            keypairs: Arc::new(Mutex::new(HashMap::new())),
+        let keyring = Ed25519Keyring {
+            keys: Arc::new(Mutex::new(HashMap::new())),
         };
 
-        let keypair = keyring.generate("test", &Algorithm::Ed25519).unwrap();
-        let keypair2 = keyring.keypair("test").unwrap();
+        let verifying_key1 = keyring.generate("test").unwrap();
+        let verifying_key2 = keyring.verifying_key("test").unwrap();
 
-        // assert_eq!(keypair.verifying_key(), keypair2.verifying_key());
+        assert_eq!(verifying_key1, verifying_key2);
     }
 }
