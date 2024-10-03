6150e53125699b319db721d44218a8a1f70c5a5e
Andrew Goldie <andrew.goldie@goldiesystems.com>
2024-10-03 16:25:24 +1300

    deferred credential test (incomplete)

---
+++ vercre-holder/tests/preauth_deferred.rs
@@ -0,0 +1,94 @@
+//! Tests for issuer-initiated pre-authorized issuance flow where the holder
+//! accepts all credentials and all claims on offer but the issuance is
+//! deferred.
+
+mod provider;
+
+use std::sync::LazyLock;
+
+use insta::assert_yaml_snapshot as assert_snapshot;
+use vercre_holder::issuance::{AcceptRequest, CredentialsRequest, OfferRequest, PinRequest};
+use vercre_issuer::{OfferType, SendType};
+use vercre_macros::create_offer_request;
+use vercre_test_utils::issuer::{self, CLIENT_ID, CREDENTIAL_ISSUER, PENDING_USER};
+
+use crate::provider as holder;
+
+static ISSUER_PROVIDER: LazyLock<issuer::Provider> = LazyLock::new(issuer::Provider::new);
+static HOLDER_PROVIDER: LazyLock<holder::Provider> =
+    LazyLock::new(|| holder::Provider::new(Some(ISSUER_PROVIDER.clone()), None));
+
+// Test end-to-end pre-authorized issuance flow, with acceptance of all
+// credentials on offer, but where the issuance is deferred.
+#[tokio::test]
+async fn preauth_deferred() {
+    // Use the issuance service endpoint to create a sample offer so we can get a
+    // valid pre-authorized code.
+    let request = create_offer_request!({
+        "credential_issuer": CREDENTIAL_ISSUER,
+        "credential_configuration_ids": ["EmployeeID_JWT"],
+        "subject_id": PENDING_USER,
+        "pre-authorize": true,
+        "tx_code_required": true,
+        "send_type": SendType::ByVal,
+    });
+
+    let offer_resp = vercre_issuer::create_offer(ISSUER_PROVIDER.clone(), request)
+        .await
+        .expect("should get offer");
+
+    let OfferType::Object(offer) = offer_resp.offer_type else {
+        panic!("expected CredentialOfferType::Object");
+    };
+
+    // Initiate the pre-authorized code flow
+    let offer_req = OfferRequest {
+        client_id: CLIENT_ID.into(),
+        subject_id: PENDING_USER.into(),
+        offer,
+    };
+    let issuance = vercre_holder::issuance::offer(HOLDER_PROVIDER.clone(), &offer_req)
+        .await
+        .expect("should process offer");
+
+    assert_snapshot!("created", issuance, {
+        ".issuance_id" => "[issuance_id]",
+        ".grants[\"urn:ietf:params:oauth:grant-type:pre-authorized_code\"][\"pre-authorized_code\"]" => "[pre-authorized_code]",
+        ".**.credentialSubject" => insta::sorted_redaction(),
+        ".**.credentialSubject.address" => insta::sorted_redaction(),
+    });
+
+    // Accept all credentials on offer
+    let accept_req = AcceptRequest {
+        issuance_id: issuance.issuance_id.clone(),
+        accept: None,
+    };
+    vercre_holder::issuance::accept(HOLDER_PROVIDER.clone(), &accept_req)
+        .await
+        .expect("should accept offer");
+
+    // Enter PIN
+    let pin_req = PinRequest {
+        issuance_id: issuance.issuance_id.clone(),
+        pin: offer_resp.tx_code.expect("should have user code"),
+    };
+    vercre_holder::issuance::pin(HOLDER_PROVIDER.clone(), &pin_req)
+        .await
+        .expect("should apply pin");
+
+    // Get available credential identifiers.
+    vercre_holder::issuance::token(HOLDER_PROVIDER.clone(), &issuance.issuance_id)
+        .await
+        .expect("should get token");
+
+    // Get (and store) credentials. Accept all on offer.
+    let cred_req = CredentialsRequest {
+        issuance_id: issuance.issuance_id.clone(),
+        ..Default::default()
+    };
+    let response = vercre_holder::issuance::credentials(HOLDER_PROVIDER.clone(), &cred_req)
+        .await
+        .expect("should get credentials");
+
+    assert!(response.deferred.is_some());
+}
